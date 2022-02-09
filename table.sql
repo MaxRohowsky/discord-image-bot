32081b05c9fa26c7a1e8645f7a1a152825f2637b
codewmax <68344959+codewmax@users.noreply.github.com>
2022-02-09 14:46:13 +0100

    Update table.sql

--- table.sql
+++ table.sql
@@ -2,4 +2,4 @@ CREATE TABLE `Counter` (
   `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `Number` int NOT NULL,
   UNIQUE KEY `Number` (`Number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
