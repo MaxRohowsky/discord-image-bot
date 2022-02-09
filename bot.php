58ec4efc0c8c30ce8f36d292213bf5e9b9b5ed7b
codewmax <68344959+codewmax@users.noreply.github.com>
2022-02-09 16:05:45 +0100

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,11 +1,11 @@
 <?PHP
-echo "bot executed";
+echo "Bot executed \n";
 
-// Server data - replace Placeholders below with your data
+// Server data - replace placeholders below with your data
 $servername = "placeholder";
-$username = "placeholder";
-$password = "placeholder";
-$dbname = "placeholder";
+$username   = "placeholder";
+$password   = "placeholder";
+$dbname     = "placeholder";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,19 +17,31 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 // Set index
-$newest_entry = "SELECT * FROM Counter ORDER BY timestamp DESC LIMIT 1";
-$newest_entry = $conn->query($newest_entry);
-$newest_entry = mysqli_fetch_assoc($newest_entry);
-$index = $newest_entry["Number"];
+$result = $conn->query("SELECT * FROM Counter");
+$row_cnt = $result->num_rows;
+// If database is empty - set 0 as initial value.
+if ($row_cnt === 0) {
+  $initial_entry = 0;
+  $sql_init = "INSERT INTO Counter (Number) VALUES ($initial_entry)";
+  $conn->query($sql_init);
+  $index = $initial_entry;
+}
+// If database is not empty - set index to highest value.
+else{
+  $newest_entry = "SELECT * FROM Counter ORDER BY Number DESC LIMIT 1";
+  $newest_entry = $conn->query($newest_entry);
+  $newest_entry= mysqli_fetch_assoc($newest_entry);
+  $index = $newest_entry["Number"];
+}
 
-// Set image to post - replace path_placeholder with path to the file that stores the images.
+//Set image to post - replace path_placeholder with path to the file that stores the images.
 $image = "path_placeholder".$index.".png";
 
 // Post to Discord - replace webhook_placeholder with your webhook.
 $webhook = "webhook_placeholder";
 $message = json_encode([
   "content" => $image,  
-  "username" => "Max's Meme Bot",
+  "username" => "Image Bot",
 ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
 
 $send = curl_init($webhook);
@@ -43,21 +55,14 @@ $output = curl_exec($send);
 curl_close($send);
 
 // Increment index and save
-$next_entry = $newest_entry["Number"]+1;
+$next_entry = $index+1;
 $sql_save = "INSERT INTO Counter (Number) VALUES ($next_entry)"; 
+$conn->query($sql_save);
 
-if ($conn->query($sql_save) === TRUE) {
-echo "New record created successfully";
-} else {
-echo "Error: " . $sql_save . "<br>" . $conn->error;
-}
-
+// Close connection to database
 $conn->close();
 
 ?>
-
-
-
 
 
 
